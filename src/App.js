fee7f49a0e3c1cf6adc81db06d3926a1a9e72813
story <story801216@yahoo.com.tw>
2021-09-29 02:25:48 +0800

    React : 按鈕計數器

--- src/App.js
+++ src/App.js
@@ -4,13 +4,29 @@ function App() {
   // [得到狀態, 設定狀態] = useState(初始值)
   const [total, setTotal] = useState(0)
   return (
-    <h1
-      onClick={() => {
-        setTotal(total + 1) // 設定狀態(目前狀態+1)
-      }}
-    >
-      {total}
-    </h1>
+    <>
+      <h1
+        onClick={() => {
+          setTotal(total + 1) // 設定狀態(目前狀態+1)
+        }}
+      >
+        {total}
+      </h1>
+      <button
+        onClick={() => {
+          setTotal(total + 1)
+        }}
+      >
+        +
+      </button>
+      <button
+        onClick={() => {
+          setTotal(total - 1)
+        }}
+      >
+        -
+      </button>
+    </>
   )
 }
 

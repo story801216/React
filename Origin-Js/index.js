f8017db81a55ee36a8a00ed55651897c0604e4d9
story <story801216@yahoo.com.tw>
2021-10-01 11:34:09 +0800

    更改位置

--- Origin-Js/index.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('add').addEventListener('click', function () {
-  document.getElementById('total').innerHTML =
-    +document.getElementById('total').innerHTML + 1
-})
-
-document.getElementById('sub').addEventListener('click', function () {
-  document.getElementById('total').innerHTML =
-    +document.getElementById('total').innerHTML - 1
-})

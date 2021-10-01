fa792ba12040574cb0e634a902863ed6345af2ac
story <story801216@yahoo.com.tw>
2021-10-02 00:58:48 +0800

    導入圖片 :補充註解

--- 教材程式碼/0928/2.img的src的使用範例/src/config.js
+++ 教材程式碼/0928/2.img的src的使用範例/src/config.js
@@ -1,5 +1,5 @@
 const debug = true
 
-export const devUrl = 'http://localhost:3000'
-export const prodUrl = 'http://www.abc.com'
+export const devUrl = 'http://localhost:3000' // 開發時的網址
+export const prodUrl = 'http://www.abc.com' // 正式上線的網址
 export const imgUrl = debug ? devUrl : prodUrl

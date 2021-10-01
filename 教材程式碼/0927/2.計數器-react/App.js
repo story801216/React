ec0d7662546ef72cf42d50bd282b485486afc82e
story <story801216@yahoo.com.tw>
2021-10-01 11:36:50 +0800

    用React寫計數器

---
+++ 教材程式碼/0927/2.計數器-react/App.js
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+function App() {
+  // [得到狀態, 設定狀態] = useState(初始值)
+  const [total, setTotal] = useState(0)
+
+  return (
+    <h1
+      onClick={() => {
+        setTotal(total + 1)
+      }}
+    >
+      {total}
+    </h1>
+  )
+}
+
+export default App

07d8d1f6c00642e099e8092fc3e5838f93dfdd16
story <story801216@yahoo.com.tw>
2021-10-02 16:58:29 +0800

    React : 函式型與類別型元件的差異-主畫面

---
+++ 教材程式碼/0929/1.計數器-類別型元件/App.js
@@ -0,0 +1,16 @@
+import CounterFC from './components/CounterFC'
+import CounterCC from './components/CounterCC'
+
+function App() {
+  return (
+    <>
+      <h2>函式型元件</h2>
+      <CounterFC />
+      <hr />
+      <h2>類別型元件</h2>
+      <CounterCC />
+    </>
+  )
+}
+
+export default App

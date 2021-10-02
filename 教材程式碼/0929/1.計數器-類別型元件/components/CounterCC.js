93d689bcd18c38b8b39c4e823df48fd47360d155
story <story801216@yahoo.com.tw>
2021-10-02 16:49:14 +0800

    React : 類別型元件的基礎樣板

---
+++ 教材程式碼/0929/1.計數器-類別型元件/components/CounterCC.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class CounterCC extends React.Component {
+  constructor() {
+    super()
+    // 定義狀態的初始值
+    this.state = {}
+  }
+
+  render() {
+    return
+    ;<></>
+  }
+}
+
+export default CounterCC

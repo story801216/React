13b94faa62fa12eff4e6432d9228fa31706493c1
story <story801216@yahoo.com.tw>
2021-10-03 00:30:46 +0800

    增加類別型寫法順序註解

--- 教材程式碼/0929/1.計數器-類別型元件/components/CounterCC.js
+++ 教材程式碼/0929/1.計數器-類別型元件/components/CounterCC.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React from 'react'
 
+// React.Component 是一個React裡的一個類別(class)
+// 使用類別型的起手式
+// 1.建構式:constructor()
+// 2.去呼叫父母類別也就是React.Component:super()
+// 3.定義狀態初始值:this.state
+// 4.再用render()方法將畫面渲染出來
 class CounterCC extends React.Component {
   constructor() {
-    super();
+    super()
     // 定義狀態的初始值
     this.state = {
       total: 0,
-    };
+    }
   }
 
   render() {
@@ -16,14 +22,14 @@ class CounterCC extends React.Component {
           onClick={() => {
             this.setState({
               total: this.state.total + 1,
-            });
+            })
           }}
         >
           {this.state.total}
         </h1>
       </>
-    );
+    )
   }
 }
 
-export default CounterCC;
+export default CounterCC

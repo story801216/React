74ba74d54a5dd7d470f81ab59f99ed99afa49cc8
story <story801216@yahoo.com.tw>
2021-10-03 01:05:39 +0800

    狀態 : 主畫面

---
+++ 教材程式碼/0929/2.狀態為物件值-類別vs函式元件/App.js
@@ -0,0 +1,14 @@
+import FormFC from './components/FormFC'
+import FormCC from './components/FormCC'
+
+function App() {
+  return (
+    <>
+      <FormFC />
+      <hr />
+      <FormCC />
+    </>
+  )
+}
+
+export default App

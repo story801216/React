1d7d58c1ce9e8d7b896e2092381b0e5a090ac7fc
story <story801216@yahoo.com.tw>
2021-10-05 00:25:39 +0800

    修改註解

--- 教材程式碼/0929/5.props-預設屬性值與解構/components/Parent.js
+++ 教材程式碼/0929/5.props-預設屬性值與解構/components/Parent.js
@@ -4,7 +4,7 @@ import Child from './Child'
 function Parent() {
   return (
     <>
-      {/* 在父母元件的render(return)中使用子女元件設定狀態 */}
+      {/* 在父母元件的render(return)中使用子女元件設定屬性 */}
       <Child text="Hello" name="Eddy" />
       <Child />
     </>

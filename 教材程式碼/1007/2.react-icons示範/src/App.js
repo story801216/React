913426b4587fbb14acd417bdb1914d1ed6b79b82
story <story801216@yahoo.com.tw>
2021-10-10 00:51:44 +0800

    導入React-icon示範

---
+++ 教材程式碼/1007/2.react-icons示範/src/App.js
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import { Button, Modal } from 'react-bootstrap'
+import { FaCartPlus } from 'react-icons/fa'
+
+function App() {
+  const [show, setShow] = useState(false)
+
+  const handleClose = () => setShow(false)
+  const handleShow = () => setShow(true)
+
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        <FaCartPlus size="2em" color="#ccc" /> 開啟
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  )
+}
+
+export default App
